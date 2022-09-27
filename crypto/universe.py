6597e6a8498a1a46460bf3ad2aedfa01a10b3765
YirenYao <53145431+YirenYao@users.noreply.github.com>
2022-09-27 12:01:48 -0400

    Fetch + update universe

---
+++ crypto/universe.py
@@ -0,0 +1,27 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+from crypto.utils import universe_path
+
+NAME_NAME = "d-lg-inline font-normal text-3xs tw-ml-0 md:tw-ml-2 md:tw-self-center tw-text-gray-500 dark:tw-text-white dark:tw-text-opacity-60"
+
+
+def fetch_universe(num):
+    res = requests.get(url='https://www.coingecko.com/')
+    res = BeautifulSoup(res.text, features="lxml")
+
+    res = res.find('div', {'class': 'coingecko-table'}).find('tbody')
+    res = res.find_all('span', {'class': NAME_NAME})[:num]
+
+    universe = {"USD": [], "BTC": []}
+    for name in res:
+        universe["USD"].append(name.text.strip() + "-USD")
+        universe["BTC"].append(name.text.strip() + "-BTC")
+
+    with open(universe_path, 'w') as universe_file:
+        json.dump(universe, universe_file)
+
+
+if __name__ == '__main__':
+    fetch_universe(50)
---
+++ crypto/utils.py
@@ -0,0 +1,3 @@
+data_path = "./data/"
+universe_path = "./data/universe.json"
+universe_size = 50
